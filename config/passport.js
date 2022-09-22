8a03a9b1bc741b7cf64c2fc3eb0a777c0272303b
Anasemos Kassahun <anasemosaka@gmail.com>
2022-09-21 21:58:26 -0400

    wip

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
+const Student = require('../models/student');
 
 
 // passport.use <-- we use this to plug-in login options
@@ -12,7 +12,29 @@ passport.use(new GoogleStrategy({
 }, function (accessToken, refreshToken, profile, cb) {
     // a user has attempted a login
     // does this user already exist in our own database?
-    // if they dont we create them
+    // lets check to see
+    Student.findOne({ googleId: profile.id }, function (err, student) {
+        // if they dont we create them
+        // check for and handle errors
+        if (err) return cb(err)
+        // if student exists in our database - log them in!
+        if (student) {
+            return cb(null, student)
+        } else {
+            // student doesnt exist, create them instead
+            const newStudent = new Student({
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                googleId: profile.id,
+            })
+            newStudent.save(function (err) {
+                if (err) return cb(err)
+                // user/student saved successful
+                return cb(null, newStudent)
+            })
+        }
+
+    })
 }))
 
 // passport.serializeUser <-- gets called one time when user logs in to create a session
